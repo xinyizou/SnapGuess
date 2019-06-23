aee0721f7ae8c0d1fee5ff9bbf117747726237a4
xinyizou <43503199+xinyizou@users.noreply.github.com>
2019-06-22 20:55:38 -0400

    working snapkit

--- app/src/main/java/com/example/snapguess/JoinFragment.java
+++ app/src/main/java/com/example/snapguess/JoinFragment.java
@@ -1,12 +1,16 @@
 package com.example.snapguess;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class JoinFragment extends Fragment {
     @Nullable
@@ -15,7 +19,19 @@ public class JoinFragment extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
 
         View view = inflater.inflate(R.layout.join_game, container, false);
+        Button play = (Button) view.findViewById(R.id.play);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                goToNextView();
+            }
+        });
 
         return view;
+    }
+
+    private void goToNextView() {
+        Intent intent = new Intent(getContext(), GameScreenActivity.class);
+        startActivity(intent);
     }
 }
